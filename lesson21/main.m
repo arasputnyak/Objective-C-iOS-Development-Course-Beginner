cdbc6734c59aee8a46b6b0560fbcbbeff044797b
Анастасия Распутняк <nastushka@MacBook-Pro-Anastasia.local>
2019-04-07 15:01:43 +0300

    task 21 added

---
+++ lesson21/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Lesson21Task
+//
+//  Created by Анастасия Распутняк on 04.04.2019.
+//  Copyright © 2019 Anastasiya Rasputnyak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
